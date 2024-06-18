9a3b149fcda134a07f8662866c453a8dc34d9f3d
Edwin Cardenas <eacardenase@gmail.com>
2024-06-18 07:08:19 -0500

    feat: add seed and install hirb gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Article.new(name: "Math Article", description: "My new description").save
+Article.create(name: "Spanish Article", description: "Hola mundo")
