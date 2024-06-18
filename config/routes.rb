5d0c9eedf56b19d0a271589c8d980ec88158ce3f
Edwin Cardenas <eacardenase@gmail.com>
2024-06-18 16:51:36 -0500

    feat: add article update resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/articles", to: "articles#create"
   get "/articles/:id", to: "articles#show", as: "article"
   get "/articles/:id/edit", to: "articles#edit", as: "article_edit"
+  patch "/articles/:id", to: "articles#update"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
