db0d762897f0e7372b8702b4a4c2c4291c0e730f
Edwin Cardenas <eacardenase@gmail.com>
2024-06-18 18:15:07 -0500

    refactor: extract article form and add before_action call

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -19,18 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
 
     redirect_to article_path(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
@@ -40,5 +38,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:name, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
