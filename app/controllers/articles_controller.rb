7aa6049d80fed97fed6bab03a1f05626328afa67
Edwin Cardenas <eacardenase@gmail.com>
2024-06-18 19:25:45 -0500

    feat: show article validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
 
-    redirect_to articles_path
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
