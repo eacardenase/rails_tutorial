3df1c3502c2176b12a064e4328908de35afd4da2
Edwin Cardenas <eacardenase@gmail.com>
2024-06-19 16:55:19 -0500

    feat: add users edit resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new", as: "signup"
   post "/signup", to: "users#create", as: "new_user_registration"
   get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "user_edit"
 
   get "/login", to: "sessions#login", as: "login"
   post "/login", to: "sessions#create", as: "new_user_session"
