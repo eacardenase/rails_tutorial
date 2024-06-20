e77558796d9afdd8ab934c7203db780727a4f62a
Edwin Cardenas <eacardenase@gmail.com>
2024-06-20 15:29:13 -0500

    feat: add migration to remove single column on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_200703) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_202747) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_200703) do
     t.boolean "is_confirmed?", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.date "birth"
     t.string "city"
     t.string "country"
