f73f59bd7b042fc8e2c1801d361bfe2f31312c60
Edwin Cardenas <eacardenase@gmail.com>
2024-06-19 20:41:07 -0500

    feat: add users and articles relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, length: { minimum: 6 }
+
+  has_many :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_014243) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_013349) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
