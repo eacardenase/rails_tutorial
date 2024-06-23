ccfa463c7d01b4c0365632a9636f7c1b29a3bd75
Edwin Cardenas <eacardenase@gmail.com>
2024-06-23 08:48:55 -0500

    feat: add token to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password # enables password and password_confirmation thanks to password_digest
+  has_secure_token :token, length: 50
 
   validates :email, presence: true
   validates :password, length: { minimum: 6 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_234833) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_23_134201) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_234833) do
     t.boolean "is_confirmed?", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   add_foreign_key "profiles", "users"
