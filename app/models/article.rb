18b81bfb43a0b812a36d9287a6cc5280a7dc6dee
Edwin Cardenas <eacardenase@gmail.com>
2024-06-23 19:14:32 -0500

    feat: ensure avatar validation on new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  validates :avatar, presence: true
 
   has_one_attached :avatar
   has_rich_text :body
